0164dbf97980f4b90753a6fab0409d395f660568
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-25 01:42:32 -0600

    Completes comment challenge

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   // add your code here
+  $('#new_comment_button').click(() => {
+    $('form#new_comment').toggle()
+  })
+
+  $('form#new_comment').submit(event => {
+    event.preventDefault()
+    let commentBody = $('textarea#comment_body').val()
+    let authorName = $('input#authorName').val()
+
+    $('#comment_list').append(
+      '<li>'
+        + commentBody +
+        '<span class="author">' + authorName +'</span></li>'
+    )
+  })
 })
